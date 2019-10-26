8ca3ed41629b945377a244a4edf12b3226394ac1
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-10-27 00:12:48 +0900

    add a new page

---
+++ jpsearch_api_access/apiAccess.js
@@ -0,0 +1,31 @@
+/* apiAccess.js */
+
+var result = null;
+var data = null;
+var keyword = "出雲";
+
+//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var httpObj = new XMLHttpRequest();
+httpObj.open("GET", "https://jpsearch.go.jp/api/item/search/jps-cross?keyword=" + encodeURIComponent(keyword), true);
+
+httpObj.onload = function() {
+	data = JSON.parse(this.responseText);
+	//console.log(data);
+	console.log("<p>Success</p>");
+	showResult(data);
+}
+
+httpObj.send(null);
+
+function showResult(data) {
+	var tag = "";
+	data.list.forEach(x => {
+		tag = tag + `<tr><td><img src="${x.common.thumbnailUrl}"></td><td>${x.id}</td><td>${x.common.title}</td><td>${x.common.linkUrl}</td></tr>`;
+	});
+	result.innerHTML = "<table>" + tag + "</table>";
+	//console.log("<table>" + tag + "</table>");
+}
+
+window.onload = function() {
+	result = document.getElementById("result");
+}
