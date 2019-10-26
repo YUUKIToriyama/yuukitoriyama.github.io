84d58f4458354f5a6abd1dddc4c8513ab246e1f4
YUUKIToriyama <ytoriyama@torisfactory.com>
2019-10-27 00:43:55 +0900

    jpsearch_api_access: 検索画面を追加

--- jpsearch_api_access/apiAccess.js
+++ jpsearch_api_access/apiAccess.js
@@ -1,21 +1,23 @@
 /* apiAccess.js */
 
-var result = null;
-var data = null;
-var keyword = "出雲";
-
-//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var httpObj = new XMLHttpRequest();
-httpObj.open("GET", "https://jpsearch.go.jp/api/item/search/jps-cross?keyword=" + encodeURIComponent(keyword), true);
-
-httpObj.onload = function() {
-	data = JSON.parse(this.responseText);
-	//console.log(data);
-	console.log("<p>Success</p>");
-	showResult(data);
+function apiAccess(input) {
+	var result = null;
+	var data = null;
+	var keyword = encodeURIComponent(input);
+	
+	//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	var httpObj = new XMLHttpRequest();
+	httpObj.open("GET", "https://jpsearch.go.jp/api/item/search/jps-cross?keyword=" + keyword, true);
+	
+	httpObj.onload = function() {
+		data = JSON.parse(this.responseText);
+		//console.log(data);
+		console.log("<p>Success</p>");
+		showResult(data);
+	}
+	
+	httpObj.send(null);
 }
-
-httpObj.send(null);
 
 function showResult(data) {
 	var tag = "";
