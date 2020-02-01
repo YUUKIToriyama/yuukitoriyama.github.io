96d910a285ccb6c242abcd42532f8f04a626c673
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-02-01 19:34:08 +0900

    Sat Feb  1 19:34:08 JST 2020

--- jpsearch_api_access/apiAccess.js
+++ jpsearch_api_access/apiAccess.js
@@ -1,33 +1,24 @@
 /* apiAccess.js */
 
-function apiAccess(input) {
-	var result = null;
-	var data = null;
-	var keyword = encodeURIComponent(input);
-	
-	//var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-	var httpObj = new XMLHttpRequest();
-	httpObj.open("GET", "https://jpsearch.go.jp/api/item/search/jps-cross?keyword=" + keyword, true);
-	
-	httpObj.onload = function() {
-		data = JSON.parse(this.responseText);
-		//console.log(data);
-		console.log("<p>Success</p>");
-		showResult(data);
-	}
-	
-	httpObj.send(null);
+window.onload = () => {
+	result = document.getElementById("result");
 }
 
-function showResult(data) {
+const apiAccess = async () => {
+	const input = document.getElementById("textbox").value;
+	const keyword = encodeURIComponent(input);
+	const url = "https://jpsearch.go.jp/api/item/search/jps-cross?keyword=" + keyword;
+	
+	const response = await fetch(url).catch(err => alert(err));
+	const json = await response.json().catch(err => alert(err));
+
+	showResult(json);
+}
+
+const showResult = (data) => {
 	var tag = "";
 	data.list.forEach(x => {
 		tag = tag + `<tr><td><img src="${x.common.thumbnailUrl}"></td><td>${x.id}</td><td>${x.common.title}</td><td>${x.common.linkUrl}</td></tr>`;
 	});
 	result.innerHTML = "<table>" + tag + "</table>";
-	//console.log("<table>" + tag + "</table>");
-}
-
-window.onload = function() {
-	result = document.getElementById("result");
 }
